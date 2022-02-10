f9415bf1cf3e4dc2990cb28ebcb02e623971d7f5
Jhonata <50640451+JhonatMiranda@users.noreply.github.com>
2022-02-10 09:22:21 -0300

    up

--- src/mapa.c
+++ src/mapa.c
@@ -1,29 +1,74 @@
 #include "mapa.h"
 
-void inicializaMapa(Apmapa* terreno){
-    terreno = (Apmapa) malloc(sizeof(mapa));
+void inicializaMapa(Apmapa *terreno)
+{
+    (*terreno) = (Apmapa)malloc(sizeof(mapa));
 }
-void fazMapaVazio(Apmapa* terreno, int x, int y){
-    int i,j;
-    (*terreno)->mat= malloc (x * y * sizeof(char));
+void fazMapaVazio(Apmapa *terreno, int x, int y)
+{
+    int i, j;
+    (*terreno)->mat = malloc(x * sizeof(char *));
+    for (i = 0; i < x; i++)
+    {
+        (*terreno)->mat[i] = malloc(y * sizeof(char));
+    }
     (*terreno)->tamanhox = x;
     (*terreno)->tamanhoy = y;
-    for (i=0;i<x;i++){
-        for (j=0;j<y;j++){
+    for (i = 0; i < x; i++)
+    {
+        for (j = 0; j < y; j++)
+        {
             (*terreno)->mat[i][j] = '.';
         }
     }
 }
-void imprimeMapa(Apmapa terreno){
-    int i,j;
-    for (i=0;i<terreno->tamanhox;i++){
-        for (j=0;j<terreno->tamanhoy;j++){
-            printf("%c",terreno->mat[i][j]);
+void imprimeMapa(Apmapa terreno)
+{
+    int i, j;
+    for (i = 0; i < terreno->tamanhox; i++)
+    {
+        for (j = 0; j < terreno->tamanhoy; j++)
+        {
+            printf("%c", terreno->mat[i][j]);
         }
+        printf("\n");
     }
 }
 
+void leArqv()
+{
+    FILE *arq;
+    char Linha[100];
+    char *result;
+    int i;
+    arq = fopen("../data/mapa1.txt", "rt");
+    if (arq == NULL)
+    {
+        printf("Problemas na abertura do arquivo\n");
+        return;
+    }
+    i = 1;
+    while (!feof(arq))
+    {
 
-int main(){
-    
+        result = fgets(Linha, 100, arq);
+        if (result)
+            // printf("Linha %d : %s", i, Linha);
+        i++;
+    }
+    printf("\n");
+    fclose(arq);
+}
+
+int main()
+{
+    // Apmapa teste;
+    // int tamanhox, tamanhoy;
+    // tamanhox = 10;
+    // tamanhoy = 10;
+    // inicializaMapa(&teste);
+    // fazMapaVazio(&teste, tamanhox, tamanhoy);
+    // imprimeMapa(teste);
+    leArqv();
+    return 0;
 }
--- src/mapa.h
+++ src/mapa.h
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct Mapa *Apmapa; 
+typedef struct Mapa *Apmapa;
 
-typedef struct Mapa{
+typedef struct Mapa
+{
     char **mat;
     int tamanhox, tamanhoy;
-}mapa;
+} mapa;
 
-void inicializaMapa(Apmapa* terreno);
-void fazMapaVazio(Apmapa* terreno, int x, int y);
+void inicializaMapa(Apmapa *terreno);
+void fazMapaVazio(Apmapa *terreno, int x, int y);
 void imprimeMapa(Apmapa terreno);
+void leArqv();
